849d52c9e62c6dc0953854a58f9cb429f013f899
chottomatta <bilmem1499@gmail.com>
2022-04-27 19:49:47 +0300

    Love you

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="love">
+        <input type="checkbox" />
+        <span></span>
+        <span></span>
+        <span></span>
+        <p></p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
